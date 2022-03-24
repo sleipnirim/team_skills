e2eecf9e42d442f1ab601ffa2eba02cce45094cb
sleipnirim <sleipnirim@gmail.com>
2022-03-24 23:32:57 +0300

    Add credentials

--- lib/screens/persons_screen.dart
+++ lib/screens/persons_screen.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:team_skills/Model/person.dart';
 import 'package:team_skills/auth_controller.dart';
 import 'package:team_skills/shared_functions.dart';
 import 'package:team_skills/storage_controller.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../widgets/person_view.dart';
 
@@ -52,33 +54,75 @@ class PersonScreen extends StatelessWidget {
         actions: SharedFunctions.appBarLoginActions(
             context, authController.auth.currentUser!),
       ),
-      body: StreamBuilder(
-        stream: storageController.persons.snapshots(),
-        builder: (BuildContext context,
-            AsyncSnapshot<QuerySnapshot<Person>> snapshot) {
-          if (snapshot.hasData) {
-            return MasonryGridView.count(
-              itemCount: snapshot.data!.size,
-              crossAxisCount: MediaQuery.of(context).size.width > 1200
-                  ? 3
-                  : MediaQuery.of(context).size.width > 800
-                      ? 2
-                      : 1,
-              shrinkWrap: true,
-              itemBuilder: (BuildContext context, int index) {
-                return PersonView(person: snapshot.data!.docs[index].data());
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            child: StreamBuilder(
+              stream: storageController.persons.snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot<Person>> snapshot) {
+                if (snapshot.hasData) {
+                  return MasonryGridView.count(
+                    itemCount: snapshot.data!.size,
+                    crossAxisCount: MediaQuery.of(context).size.width > 1200
+                        ? 3
+                        : MediaQuery.of(context).size.width > 800
+                            ? 2
+                            : 1,
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      return PersonView(
+                          person: snapshot.data!.docs[index].data());
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return const Center(
+                    child: Text('Error loading persons'),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
               },
-            );
-          } else if (snapshot.hasError) {
-            return const Center(
-              child: Text('Error loading persons'),
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              RichText(
+                text: TextSpan(
+                  style: const TextStyle(color: Colors.black38),
+                  children: [
+                    const TextSpan(
+                      text: 'Created by Vladislav Zhukovskii with ',
+                    ),
+                    TextSpan(
+                      text: 'Flutter',
+                      style: const TextStyle(fontStyle: FontStyle.italic),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          launch('https://flutter.dev');
+                        },
+                    ),
+                    const TextSpan(text: '. '),
+                    TextSpan(
+                        text: 'Source',
+                        style: const TextStyle(fontStyle: FontStyle.italic),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            launch('https://github.com/sleipnirim/team_skills');
+                          }),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 5,
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
